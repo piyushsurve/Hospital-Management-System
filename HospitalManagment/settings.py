3ba6228d618e4df6fafddaa101b6caf0936810dc
piyushsurve <142381118+piyushsurve@users.noreply.github.com>
2026-07-13 20:32:23 -0700

    Update settings.py
    Database Settings changed again based on the render

--- HospitalManagment/settings.py
+++ HospitalManagment/settings.py
@@ -77,18 +77,13 @@ WSGI_APPLICATION = 'HospitalManagment.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': '5432',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
